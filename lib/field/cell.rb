bcc6752fc26eb6868ecea3f6c64c5c30a5a3f9a8
ledsun <shigeru.nakajima@gmail.com>
2023-12-30 15:48:12 +0900

    refactor: Delete methods that are not yet used.

--- lib/field/cell.rb
+++ lib/field/cell.rb
@@ -4,23 +4,7 @@ class Cell
   def initialize(x, y)
     @x = x
     @y = y
-    @has_block = false
-    @color = nil
   end
 
-  def render?
-    @has_block
-  end
-
-  def set_block(color)
-    @has_block = true
-    @color = color
-  end
-
-  def remove_block
-    return unless @has_block
-
-    @has_block = false
-    @color = nil
-  end
+  def render? = false
 end
